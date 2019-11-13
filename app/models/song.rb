dc7a327889aae55b4baf305d545a44649132f667
markenright <enright.mark@gmail.com>
2019-11-13 13:34:50 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,7 @@
 class Song < ActiveRecord::Base
 
+    belongs_to :artist   #artist has many songs
+    belongs_to :genre   #genre has many songs
+    
+
 end
--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,9 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+    def change
+        create_table :songs do |t|   #join table of artists and genres, many to many
+            t.string :name
+            t.integer :artist_id
+            t.integer :genre_id
+        end
+    end
 end
